57fa708aeb99b640736bd8c54a1b61ec2c609b3f
TTeuZ <paulomateus.luza@hotmail.com>
2020-08-08 00:51:42 -0300

    footer da home

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-container fluid> </v-container>
+  <v-container fluid class="ma-0 pa-0">
+    <div id="footer">
+      <span id="footer-title">PLK - Feito por Paulo Mateus Luza Alves</span>
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -9,3 +13,31 @@ export default {
   middleware: 'auth'
 }
 </script>
+
+<style scoped>
+#footer {
+  height: 80px;
+  width: 100%;
+
+  background-color: orange;
+
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  bottom: 0%;
+}
+
+#footer-title {
+  font-family: 'Exo Regular';
+  font-size: 18px;
+}
+
+@media screen and (max-width: 350px) {
+  #footer-title {
+    font-size: 16px;
+  }
+}
+</style>
